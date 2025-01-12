285c3d0bcfbd13ec1ab6c0a72935425fe497356a
Gabriel de Pauli Santos <gabrieldepauli.ads@gmail.com>
2025-01-12 17:05:56 -0300

    Commit inicial do projeto.

---
+++ app/src/main/java/br/edu/ifsp/dmo1/logindatastore/ui/logged/LoggedActivity.kt
@@ -0,0 +1,22 @@
+package br.edu.ifsp.dmo1.logindatastore.ui.logged
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import br.edu.ifsp.dmo1.login_datastore.ui.logged.LoggedViewModel
+import br.edu.ifsp.dmo1.logindatastore.databinding.ActivityLoggedBinding
+
+class LoggedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoggedBinding
+    private lateinit var viewModel: LoggedViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoggedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(LoggedViewModel::class.java)
+        binding.textMessage.setText("Bem-Vindo")
+    }
+
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo1/logindatastore/ui/logged/LoggedViewModel.kt
@@ -0,0 +1,6 @@
+package br.edu.ifsp.dmo1.login_datastore.ui.logged
+
+import androidx.lifecycle.ViewModel
+
+class LoggedViewModel : ViewModel() {
+}
